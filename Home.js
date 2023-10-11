b937224d5f487e3d299bfdb533a1c17b88894924
Sasidhar-522101 <143772860+Sasidhar-522101@users.noreply.github.com>
2023-10-11 16:25:03 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Home=()=>{
+    return(
+        <div>
+            <center>
+                <h4>Home Page</h4>
+            </center>
+        </div>
+    )
+}
+export default Home;
